7eb844ce2592b07957e6aa00199af9f40bfe3f3d
kmkangutkar <kmkangutkar@users.noreply.github.com>
2017-10-27 15:18:06 +0530

    Add maze options

--- parameters.py
+++ parameters.py
@@ -1,12 +1,13 @@
 # graphics
-USE_GUI = True
+USE_GUI = False
 USE_RENDER = True
 BLOCK_SIZE = 40 # pixels
 
 # maze
 START = (0, 0)
-GOAL = (19, 0)
-MAZE_FILE = 'example_maze2' # 'tmp/example_maze'
+GOAL = (0, 15)
+MAZE_CHOICES = ['tmp/example_maze', 'example_maze2', 'tmp/example_maze_not_working']
+MAZE_FILE = MAZE_CHOICES[0]
 ACTION_SPACE = ['up', 'down', 'left', 'right']
 
 # learning
